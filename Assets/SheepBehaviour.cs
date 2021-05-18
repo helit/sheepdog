9aba57c0323d43bea9e4aecf16566139acb1829a
henlit <henrik.littke@gmail.com>
2021-05-18 22:59:55 +0200

    add sheep awareness

--- Assets/SheepBehaviour.cs
+++ Assets/SheepBehaviour.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class SheepBehaviour : MonoBehaviour
 {
-  public float perceptionRadius = 50f;
+  public enum Status { Idling, Fleeing }
+  public Status status;
+  public float perceptionRadius = 10f;
+  public GameObject player;
 
   // Start is called before the first frame update
   void Start()
@@ -15,6 +18,27 @@ public class SheepBehaviour : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    if (Vector3.Distance(transform.position, player.transform.position) <= perceptionRadius)
+    {
+      status = Status.Fleeing;
+    }
+    else
+    {
+      status = Status.Idling;
+    }
+  }
 
+  void OnDrawGizmos()
+  {
+    if (status == Status.Fleeing)
+    {
+      Gizmos.color = Color.red;
+    }
+    else if (status == Status.Idling)
+    {
+      Gizmos.color = Color.white;
+    }
+
+    Gizmos.DrawWireSphere(transform.position, perceptionRadius);
   }
 }
